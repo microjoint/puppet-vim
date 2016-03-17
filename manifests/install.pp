294a5b594de93f35e998aa880af169c532e546a1
Marcus Campbell <marcus.campbell@williamhill.com>
2016-03-17 11:16:14 +0100

    .

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,11 +17,11 @@ class vim::install inherits vim {
   # sort out the vim colors, when we clone it already has a
   # 'colors' sub-directory
 
-  exec{'move_vim_colors':
-    path        => ['/usr/bin'],
-    cwd         => '/usr/share/vim/vim74/colors',
-    command     => 'rsync -a colors/ .',
-    subscribe   => Vcsrepo['/usr/share/vim/vim74'],
-    refreshonly => true,
-  }
+  #exec{'move_vim_colors':
+  #  path        => ['/usr/bin'],
+  #  cwd         => '/usr/share/vim/vim74/colors',
+  #  command     => 'rsync -a colors/ .',
+  #  subscribe   => Vcsrepo['/usr/share/vim/vim74'],
+  #  refreshonly => true,
+  #}
 }
