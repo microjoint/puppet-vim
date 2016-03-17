c9e02a3486d67219e45ac038829bd49b847b929f
Marcus Campbell <marcus.campbell@williamhill.com>
2016-03-17 16:36:42 +0100

    .

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,19 +7,20 @@ class vim::install inherits vim {
 
   create_resources( package, $package, $defaults )
 
-  vcsrepo{ '/usr/share/vim/vim74':
+  vcsrepo{ '/usr/share/repo/colors':
     ensure   => latest,
     force    => true,
     source   => 'https://github.com/flazz/vim-colorschemes.git',
     provider => git,
+    require  => File['/usr/share/repo'],
   }
 
   # sort out the vim colors, when we clone it already has a
   # 'colors' sub-directory
 
-  file{ '/usr/share/vim/vim74/colors':
-    ensure => link,
-    target => '/usr/share/repo/colors/colors',
-    force  => true,
-  }
+  #file{ '/usr/share/vim/vim74/colors':
+  #  ensure => link,
+  #  target => '/usr/share/repo/colors/colors',
+  #  force  => true,
+  #}
 }
